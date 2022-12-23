2967864e3476abc455ac31da0febba4687f85e41
clearfountain <mjedet1001@gmail.com>
2022-12-23 19:58:29 +0100

    Removed unused usings in CatalogContext

--- src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -1,10 +1,6 @@
 ﻿using Catalog.Api.Entities;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.Api.Data
 {
